f5ddf5608eaa94f5ed0cb041f14d716098c80e4a
ZiLem <42595478+zilem@users.noreply.github.com>
2018-08-24 02:31:01 +0800

    24/9 02:30 upload

--- handler.php
+++ handler.php
@@ -17,7 +17,7 @@
 
   $headers = "From: $email_from \r\n";
 
-  $headers .= "Reply-To: $visitor_email \r\n";
+  $headers = "Reply-To: $visitor_email \r\n";
 
   mail($to,$email_subject,$email_body,$headers);
 
