fcdfb0be4336d0c1522b5152a6d44162663f70a2
ZiLem <42595478+zilem@users.noreply.github.com>
2018-08-24 01:55:54 +0800

    24/8 01:55 upload

---
+++ handler.php
@@ -0,0 +1,26 @@
+<?php
+  $firstname = $_POST['firstname'];
+  $lastname = $_POST['lastname'];
+  $visitor_email = $_POST['email'];
+  $message = $_POST['message'];
+
+  $email_from = 'story@zilem.studio';
+
+  $email_subject = "New Form Submission";
+
+  $email_body = "User FirstName: $firstname.\n".
+                  "User LastName: $lastname.\n".
+                    "User Email: $visitor_email.\n".
+                      "User Message: $message.\n";
+
+  $to = "story@zilem.studio";
+
+  $headers = "From" $email_from \r\n";
+
+  $headers .= "Reply-To: $visitor_email \r\n";
+
+  mail($to,$email_subject,$email_body,$headers);
+
+  header("Location: index.html");
+
+$ ?>
