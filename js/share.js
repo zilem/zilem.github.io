7827e9c0b65737ece59a61e97ce138be75da2f7a
ZiLem <42595478+zilem@users.noreply.github.com>
2018-09-12 17:20:32 +0800

    12/09 17:20 upload

--- js/share.js
+++ js/share.js
@@ -2,6 +2,7 @@ function shareFunction(shareID) {
   var box = document.getElementById(shareID);
   box.classList.toggle("show");
 }
+// SHARING PAGE
 function wHShare(post_title) {
   window.location = 'whatsapp://send?text=' + post_title + '%0A' + location.href.replace(location.hash,"");
 }
@@ -52,4 +53,27 @@ function gShare(post,post_title) {
 function eShare(post,post_title) {
   var byname = '%0Aby Zi R. Lem, Screenwriter-Filmmaker%0A';
   window.open ('mailto:?subject=' + post_title + '&body=Thought you might like this: %0A%0A' + location.href.replace(location.hash,"") + "#" + post + "%0A%0A" + document.getElementsByTagName("meta")[5].content + "%0A%0A" + byname);
+}
+
+// SHARING INTRO
+function wIShare(post_title) {
+  window.location = 'whatsapp://send?text=' + post_title + '%0A' + window.location.hostname + '/intro.html';
+}
+function tIShare(post_title) {
+  window.open ('https://twitter.com/intent/tweet?text=' + post_title + '%0A' + window.location.hostname + '/intro.html');
+}
+function mIShare(post_title) {
+  window.open ('fb-messenger://share?link=' + post_title + '%0A' + window.location.hostname + '/intro.html');
+}
+function fIShare(post_title) {
+  window.open ('https://facebook.com/sharer/sharer.php?u=' + post_title + '%0A' + window.location.hostname + '/intro.html');
+}
+function lIShare(post_title) {
+  window.open ('https://linkedin.com/shareArticle?mini=true&url=' + post_title + '%0A' + window.location.hostname + '/intro.html' + "%0A%0A" + '&summary=' + document.getElementsByTagName("meta")[5].content);
+}
+function gIShare(post_title) {
+  window.open ('https://plus.google.com/share?url=' + post_title + '%0A' + window.location.hostname + '/intro.html');
+}
+function eIShare(post_title) {
+  window.open ('mailto:?subject=' + post_title + '&body=Thought you might like this: %0A%0A' + window.location.hostname + '/intro.html' + "%0A%0A" + 'by Zi R. Lem, Screenwriter-Filmmaker');
 }
